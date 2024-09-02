509c8293c0e5ff2514565eb0cd367e21663e4ccb
H3nriqueL1ma <henriquecarlos361893@gmail.com>
2024-09-01 21:23:19 -0300

    feat: Adição de rotas de registro de sessão e registro de cliente do microsserviço de Registro.

--- src/main/java/com/github/h3nriquel1ma/nexbankapigateway/Configs/GatewayConfig.java
+++ src/main/java/com/github/h3nriquel1ma/nexbankapigateway/Configs/GatewayConfig.java
@@ -5,14 +5,20 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Configuration;
 
+// Gateway - Aplicação para direcionar as requisições à seus devidos serviços.
 @Configuration
 public class GatewayConfig {
 
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("register_route", p -> p
-                        .path("/register-user")
-                        .uri("http://localhost:8010/register"))
+                // Rotas de Registro
+                .route("register_session_route", p -> p
+                        .path("/register-session")
+                        .uri("lb://register-service/register/session"))
+                .route("register_client_route", p -> p
+                        .path("/register-client")
+                        .uri("lb://register-service/register/client"))
+                // Rotas de Autenticação
                 .route("authentication_route", p -> p
                         .path("/authenticate-user")
                         .uri("http://localhost:8020/authentication"))
